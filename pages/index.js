9ae892cfa8b4bb09582a99219fe58a3d3645aac4
charlotte-hues <hues.charlotte@gmail.com>
2021-05-14 11:41:41 +0100

    Added title and description to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Head from 'next/head';
+
 import Homepage from '../components/sections/Home/Home';
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <meta
+        name="description"
+        content="Creative Developer | Designer | Liverpool"
+      />
+      <title>Charlotte Hughes</title>
+    </Head>
     <Homepage />
+    </>
   )
 }
