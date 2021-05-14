17cf300de179f0a02f03e36fa6e3864956c0cd5d
charlotte-hues <hues.charlotte@gmail.com>
2021-05-14 21:29:22 +0100

    moved logic from useContactForm directly into index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,46 @@
 import Head from 'next/head';
+import {useState} from 'react';
 
 import Homepage from '../components/sections/Home/Home';
-import useContactForm from '../components/shared/hooks/useContactForm';
 
 export default function Home() {
 
-  const contactForm = useContactForm();
+  const [formSuccess, setFormSuccess] = useState(false);
+  const [formError, setFormError] = useState(false);
+
+    const handleSubmit = (data) => {
+    fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then((res) => {
+        console.log(res)
+        if (res.status === 200) {
+        setFormSuccess(true);
+        } else {setFormError(true)}
+    })
+    }
+
+    const handleErrorConfirm = (e) => {
+    e.preventDefault();
+    setFormError(false);
+    };
+
+    const handleSuccessConfirm = (e) => {
+    e.preventDefault();
+    setFormSuccess(false);
+    };
+
+    const contactForm = {
+        handleSubmit: handleSubmit,
+        handleSuccess: handleSuccessConfirm,
+        handleError: handleErrorConfirm,
+        formSuccess: formSuccess,
+        formError: formError
+    }
 
   return (
     <>
