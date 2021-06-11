bbd8ff4189b1f95424310ee27d3e5fa22c4e29f3
charlotte-hues <hues.charlotte@gmail.com>
2021-06-11 13:43:28 +0100

    Added a fallback for when no paths match

--- pages/[home]/index.js
+++ pages/[home]/index.js
@@ -12,16 +12,20 @@ export default function Home() {
 
   const location = useRouter();
 
+  console.log(location.query);
+
   const metaData = routes.find(r => r.path === location.asPath);
+
+  console.log(metaData);
 
   return (
     <>
     <Head>
       <meta
         name="description"
-        content={metaData.description}
+        content={metaData ? metaData.description : "Designer and Creative Developer based in the UK"}
       />
-      <title>{metaData.title}</title>
+      <title>{metaData ? metaData.title : "Charlotte Hughes Designer and Creative Developer"}</title>
     </Head>
     <Homepage contactForm={contactForm} />
     </>
